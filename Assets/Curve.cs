20d52bfdaa2f55ba29f29cfa35de95d3413c8ea7
Wardcore <40540898+Wardcore@users.noreply.github.com>
2019-03-27 09:47:53 +0100

    merge

---
+++ Assets/Curve.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curve : MonoBehaviour {
+
+	public Transform Shootpoint;
+	public float Shotpower = 2;
+	public float ShotRangeFactor = 2;
+	public Vector3[] points;
+
+	public LineRenderer m_line;
+	public int LineDivision = 10;
+
+	void Start()
+	{
+		if(!m_line)
+			m_line = GetComponent<LineRenderer>();
+		m_line.positionCount = LineDivision;
+	}
+
+	void Update()
+	{
+		points[0] = Shootpoint.position;
+		points[1] = Shootpoint.position + Shootpoint.forward * Shotpower;
+		points[2] = points[1];
+		points[3] = transform.position + transform.forward * Shotpower*ShotRangeFactor;
+
+		for (int i = 0; i < m_line.positionCount; i++)
+		{
+			print(GetPoint((1/LineDivision)*i));
+			m_line.SetPosition(i, GetPoint((1/LineDivision)*i));
+		}
+	}
+
+	// void OnValidate()
+	// {
+	// 	points[0] = Shootpoint.position;
+	// 	points[1] = Shootpoint.position + Shootpoint.forward * Shotpower;
+	// 	points[2] = points[1];
+	// 	points[3] = transform.position + transform.forward * Shotpower*ShotRangeFactor;
+	// }
+	public void Reset(){
+		points = new Vector3[]{
+			new Vector3(1f, 0f, 0f),
+			new Vector3(2f, 0f, 0f),
+			new Vector3(3f, 0f, 0f),
+			new Vector3(4f, 0f, 0f)
+		};
+	}
+
+	public Vector3 GetPoint(float t){
+		return transform.TransformPoint(Bezier.GetPoint(points[0],points[1],points[2], points[3], t));
+	}
+	public Vector3 GetVelocity(float t){
+		return transform.TransformPoint(Bezier.GetFirstDerivative(points[0],points[1],points[2], points[3], t)) - transform.position;
+	}
+
+	public Vector3 GetDirection(float t){
+		return GetVelocity(t).normalized;
+	}
+}
---
+++ Assets/CurveWalker.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SplineWalkerMode {
+	Once,
+	Loop,
+	PingPong
+}
+
+public class CurveWalker : MonoBehaviour {
+
+	public Curve m_curve;
+	public float m_duration;
+	public SplineWalkerMode mode;
+	private float progress;
+
+	private bool goingForward = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(goingForward){
+			progress += Time.deltaTime / m_duration;
+
+			if(progress > 1){
+				switch (mode)
+				{
+					case SplineWalkerMode.Loop:
+						progress -= 1f;
+					break;
+					case SplineWalkerMode.Once:
+						progress = 1f;
+					break;
+					case SplineWalkerMode.PingPong:
+						progress = 2f - progress;
+						goingForward = false;
+					break;
+					default:
+						progress = 1f;
+					break;
+				}
+			}
+		}
+		else{
+			progress -= Time.deltaTime / m_duration;
+			if(progress < 0f){
+				progress = -progress;
+				goingForward = true;
+			}
+		}
+		
+		transform.localPosition = m_curve.GetPoint(progress);
+	}
+}
